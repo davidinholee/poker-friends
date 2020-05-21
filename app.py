0056611ed50fdb9586777202beb03a51e55893e9
David Inho Lee <davidinholee@gmail.com>
2020-05-21 05:11:47 -0400

    Background done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__, template_folder="resources/templates/")
+app = Flask(__name__, template_folder="resources/templates/", static_folder="resources/static/")
 
 @app.route("/")
 def index():
